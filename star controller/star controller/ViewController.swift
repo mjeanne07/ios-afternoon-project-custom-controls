2a2b5c849702f9bed19b977052c0cbf3d37a5052
morgan smith <mssmitty07@gmail.com>
2020-04-16 20:58:11 -0400

    initial commit

---
+++ star controller/star controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  star controller
+//
+//  Created by Morgan Smith on 4/16/20.
+//  Copyright © 2020 Morgan Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
